bced6d40ce6d18c40c9e3301ace7aca156aee03c
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-03-13 12:47:03 +0000

    Delete body-parser because deprecated

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,47 @@
 const express = require('express');
-
-const app = express();
-
 const cors = require('cors');
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// req.body.name
+// To make unique IDs for each web project
+const { v4: uuidv4 } = require('uuid');
 
-const fs = require('fs');
-
-// Specify the port number as a variable.
-const PORT = process.env.PORT || 8080;
-
-// Store the route in a variable
-const ROUTE = '/api';
+const app = express();
 
 // Allow cross-origin requests from all origins
 app.use(cors())
 
+// Body-parser is deprecated, use express.json() to access data in the body of the HTTP requests instead
+app.use(express.json());
 
-// An array of web project items
+// Specify the port number as a variable for development and production environments
+const PORT = process.env.PORT || 8080;
+
+// Store the API route as a variable
+const ROUTE = '/api';
+
+
+// Initialise array of default web project data
 const webProjects = [
     {
-        id: "1",
+        id: uuidv4(),
         title: "React Game!",
         description: "Tic tac toe game created using 'Create React App'.",
         url: "http://heroku/myapp/game/"
     },
     {
-        id: "2",
+        id: uuidv4(),
         title: "Online store",
         description: "Online store created with HTML, CSS and JavaScript.",
         url: "https://git.com/myrepos/shop/index"
     }
 ];
 
+
+/*  ROUTING METHODS */
+
 // Return information on all web projects
 app.get(ROUTE, (req, res) => {
-    console.log("GET request executed!");
-    res.send(JSON.stringify(webProjects, null, "\n"));
+    console.log("GET request received!");
+    res.send(JSON.stringify(webProjects));
 })
 
 // Add a new web project
@@ -48,10 +49,11 @@ app.post(ROUTE, (req, res) => {
 
     // Create an object literal with the POST query data
     const newProject = {
-        id: req.query.id,
-        title: req.query.title,
-        description: req.query.description,
-        url: req.query.url
+        // Create a UUID in the Express app, not the frontend
+        id: uuidv4(),
+        title: decodeURIComponent(req.query.title),
+        description: decodeURIComponent(req.query.description),
+        url: decodeURIComponent(req.query.url)
     }
 
     // To avoid duplicate project data, position in array is derived from id provided.  
@@ -69,12 +71,23 @@ app.post(ROUTE, (req, res) => {
 // Delete a web project 
 app.delete(ROUTE, (req, res) => {
 
-    // Edit the array of web projects using the id provided in the query string 
-    webProjects.splice(Number(req.query.id) - 1, 1);
+    console.log(req.query);
 
-    res.write("DELETE request executed!");
-    res.write("\nWeb projects:\n" + JSON.stringify(webProjects, null, '\n\t'));
-    res.end();
+    // Find the web project in the array using the ID parameter
+    
+    const index = webProjects.findIndex(project => project.id === req.query.id);
+
+    // If the web project exists, remove it from the array
+    if (index !== -1) {
+        webProjects.splice(index, 1);
+        res.write("DELETE request executed!");
+        res.write("\nWeb projects:\n" + JSON.stringify(webProjects, null, '\n\t'));
+        res.end();
+    } else {
+        // If the web project doesn't exist, return a 404 error
+        res.status(404).send('Web project not found');
+    }
+
 })
 
 // Edit a web project 
@@ -97,5 +110,7 @@ app.put(ROUTE, (req, res) => {
 
 })
 
-// Listen for HTTP requests on the given port.
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+/* END OF ROUTING METHODS */
+
+// Listen for HTTP requests on the port specified
+app.listen(PORT, () => console.log(`Listening for HTTP requests on port ${PORT}`));
